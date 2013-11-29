6e2d6b4db735fc49649ba74e4fc1b8643b243599
Ivan <kia84@users.noreply.github.com>
2013-11-29 09:57:42 +0300

    Create fpoint.h

---
+++ fpoint.h
@@ -0,0 +1,23 @@
+#ifndef FPOINT_H
+#define FPOINT_H
+
+#include <QMainWindow>
+
+namespace Ui {
+    class FPoint;
+}
+
+class FPoint : public QPoint
+{
+
+public:
+    FPoint(int x, int y);
+    FPoint(int x, int y, bool is_wall);
+    ~FPoint();
+    int x;
+    int y;
+    bool is_wall = false;
+    FPoint points[8];
+};
+
+#endif // FPOINT_H
